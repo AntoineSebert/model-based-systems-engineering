5c146af1c434272ee63ea70fc5e935152397ef5a
s163950 <s163950@student.dtu.dk>
2020-11-19 15:26:01 +0100

    remove model artifacts

--- src/model.py
+++ src/model.py
@@ -3,6 +3,7 @@ from typing import List
 from xml.etree import ElementTree
 from dataclasses import dataclass
 
+
 @dataclass
 class Device:
     name: str
@@ -24,52 +25,6 @@ class Switch(Device):
 @dataclass(eq=False)
 class EndSystem(Device):
     pass
-
-    # def __eq__(self, other):
-    #     # Only equal by name (ID)
-    #     if self.name == other.name:
-    #         return True
-    #     return False
-    #
-    # def __hash__(self):
-    #     # print(hash(str(self)))
-    #     return hash(str(self.name))
-
-
-class streamInstance:
-    def __init__(self):
-        self.instance = 0
-
-    @staticmethod
-    def getInstance(self):
-        self.instance = self.instance + 1
-        return self.instance
-
-
-class Stream:
-    def __init__(self, id: str, src: Device, dest: Device, size: int, period: int, deadline: int, rl: int):
-        self.id = id
-        self.src = src
-        self.dest = dest
-        self.size = size
-        self.period = period
-        self.deadline = deadline
-        self.rl = rl
-
-    @classmethod
-    def from_element(self, element: ElementTree):
-        """
-        Assuming element is structured as:
-            <stream id="Stream1" src="ES2" dest="ES4" size="100" period="1000" deadline="10000" rl="2"/>
-        """
-        return self(**element.attrib)
-
-
-# Is a sequence of steps on the form {(ES2 --> SW0), (SW0 --> ES3)}
-class Route:
-    def __init__(self, stream: Stream, route: List[Tuple[str, str]]):
-        self.stream = stream
-        self.route = route
 
 
 # Solution output is:
